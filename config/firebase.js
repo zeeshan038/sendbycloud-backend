1a80be16b5a6029cc4249b1132c10f577ef8037f
zeeshan038 <hankhan123623@gmail.com>
2026-03-13 18:53:48 +0500

    Fix Firebase credential property names to snake_case

--- config/firebase.js
+++ config/firebase.js
@@ -2,10 +2,11 @@ import admin from "firebase-admin";
 
 
 const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
 };
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
